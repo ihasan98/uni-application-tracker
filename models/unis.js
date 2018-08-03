55458664c7bedbde6423c507705d29a5510985b3
Weiran Gu <weirangu@gmail.com>
2018-08-02 20:39:12 -0400

    Essays are now a subdocument under the main document.

--- models/unis.js
+++ models/unis.js
@@ -1,21 +1,21 @@
 var mongoose = require("mongoose");
 
-var schema = new mongoose.Schema({
+var essaySchema = new mongoose.Schema({
+	name: String
+});
+
+var uniSchema = new mongoose.Schema({
 	name: {
 		type: String,
 		required: [true, "You have to enter a name."]
 	},
 
-	essays: [
-		{
-			name: String
-		}
-	],
+	essays: [ essaySchema ]
 });
 
 // Defines a virtual property for the number of essays
-schema.virtual('essays_num').get(function () {
+uniSchema.virtual('essays_num').get(function () {
 	return this.essays.length;
 });
 
-module.exports = mongoose.model("Universities", schema);
+module.exports = mongoose.model("University", uniSchema);
