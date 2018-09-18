bde74c8a736cb9d8caf24b290158ee249e9e3fe3
Weiran Gu <weirangu@gmail.com>
2018-09-17 22:27:42 -0400

    Added login route.

--- API/routes/api/index.js
+++ API/routes/api/index.js
@@ -3,10 +3,12 @@ const 	express			= require("express"),
 		router			= express.Router(),
 		users			= require("./users"),
 		unis			= require("./unis"),
-		essays			= require("./essays");
+		essays			= require("./essays"),
+		login			= require("./login");
 
 router.use("/essays", essays);
 router.use("/unis", unis);
 router.use("/users", users);
+router.use("/login", login);
 
 module.exports = router;
