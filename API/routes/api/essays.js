c02001cfad26a6d4c65209e4b025c799e7fde936
Weiran Gu <weirangu@gmail.com>
2018-09-30 22:30:07 -0400

    Added basic authentication for essays and unis.

--- API/routes/api/essays.js
+++ API/routes/api/essays.js
@@ -1,11 +1,12 @@
 // Importing the required packages
 const 	express			= require("express"),
 		router			= express.Router(),
-		essays			= require("../../helpers/essays");
+		essays			= require("../../helpers/essays"),
+		auth			= require("../../helpers/auth");
 
 router.route("/:essay_id")
-.get(essays.getEssay)
-.put(essays.putEssay)
-.delete(essays.deleteEssay);
+.get(auth.isLoggedInRoute(), essays.getEssay)
+.put(auth.isLoggedInRoute(), essays.putEssay)
+.delete(auth.isLoggedInRoute(), essays.deleteEssay);
 
 module.exports = router;
