7e911e9720856719450377fb9210e06a2893abd4
Weiran Gu <weirangu@gmail.com>
2018-09-30 22:37:50 -0400

    Updated style guide and cleaned up code.

--- API/models/user.js
+++ API/models/user.js
@@ -1,6 +1,6 @@
-const mongoose 				= require("mongoose"),
-	  passportLocalMongoose = require("passport-local-mongoose");
-	  
+const 	mongoose 				= require("mongoose"),
+		passportLocalMongoose	= require("passport-local-mongoose");
+
 const userSchema = new mongoose.Schema({
 	email: {
 		type: String,
