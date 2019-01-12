658e000d5e22b3c3dd5ab3ec9d19a8d0540b7822
Ibrahim Hasan <ibrahim.hasan@mail.utoronto.ca>
2019-01-12 15:05:41 -0500

    Connected Menubar to new redux auth

--- react-frontend/src/components/MenuBar.js
+++ react-frontend/src/components/MenuBar.js
@@ -1,34 +1,21 @@
 import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
+import { connect } from "react-redux";
+import { logout } from "../store/actions/auth";
 import { Button } from '@material-ui/core';
 
 class MenuBar extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            isLoggedIn: props.isLoggedIn,
-            username: props.username
-        }
-        this.logout = this.props.logout;
-    }
-
-    componentDidUpdate(prevProps) {
-        // The if statements are necessary, otherwise we enter an infinite loop
-        console.log(
-            "menubar update"
-        )
-        if (this.props.isLoggedIn !== prevProps.isLoggedIn) {
-            this.setState({ isLoggedIn: this.props.isLoggedIn })
-        }
-        if (this.props.username !== prevProps.username) {
-            this.setState({ username: this.props.username });
-        }
-    }
+    logout = e => {
+        e.preventDefault();
+        this.props.logout();
+        this.props.history.push("/login");
+    };
 
     render() {
-        var usernameText
-        var logoutButton
-        if (this.state.isLoggedIn) {
-            usernameText = "Welcome " + this.state.username;
+        let usernameText
+        let logoutButton
+        if (this.props.currentUser.isAuthenticated) {
+            usernameText = "Welcome " + this.props.currentUser.user.username;
             logoutButton = <Button onClick={this.logout}> Logout </Button>
         } else {
             usernameText = "Please log in!"
@@ -41,4 +28,12 @@ class MenuBar extends Component {
     }
 }
 
-export default MenuBar;
+function mapStateToProps(state) {
+    return {
+      currentUser: state.currentUser
+    };
+  }
+  
+export default withRouter(
+    connect(mapStateToProps, { logout })(MenuBar)
+);
